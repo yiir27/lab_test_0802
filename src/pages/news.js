32837bd68ff7cb59ea2403e98c019ecd1ce071d1
yiir27 <camp.yui.c@gmail.com>
2023-08-02 16:09:51 +0900

    ファイル一式

---
+++ src/pages/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "../styles/test.module.scss";
+
+const news = () => {
+  return (
+    <div className='text-5xl underline'>
+        news
+        <p className={style.test}>テスト</p>
+    </div>
+  )
+}
+
+export default news
