38d3512f0a7d6ebbb8c1a02c4c09b965d0eac357
yiir27 <camp.yui.c@gmail.com>
2023-08-09 12:10:52 +0900

    PWAを追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  register: true,
+  skipWaiting: false,
+  runtimeCaching,
+  // reactStrictMode: true,
+});
+const NextConfig = withPWA({});
+
+module.exports = NextConfig;
